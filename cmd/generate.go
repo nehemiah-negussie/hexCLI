fad536052a61d487d9ef0743073b0edb5bdc1a5e
Nehemiah Negussie <46787242+nehemiah-negussie@users.noreply.github.com>
2022-12-22 10:15:32 -0500

    Cleaned up code and wrote some comments

--- cmd/generate.go
+++ cmd/generate.go
@@ -70,7 +70,6 @@ func fixAngle (angle int) (fixed int) {
 	}
 	
 	if angle < 0 {
-		fixed = angle
 		for fixed < 0{
 			fixed += 360
 		}
@@ -78,7 +77,6 @@ func fixAngle (angle int) (fixed int) {
 	}
 
 	if angle > 0 {
-		fixed = angle
 		for fixed >= 360{
 			fixed -= 360
 		}
@@ -97,14 +95,14 @@ var generateCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		// Seed randomness with time
 		rand.Seed(time.Now().UnixNano())
+
 		// Get the value of the flag
 		scheme, _ := cmd.Flags().GetString("scheme")
-		// [testing] show if input was taken
-		if scheme != "" {
-			fmt.Println("Test")
-		}
+
+		// Define palette array
 		var palette[5]HSVColor
-		// Switch statement for each color scheme (algorithms to be added in the future)
+
+		// Switch statement for each color scheme
 		switch scheme {
 			case "":
 				fmt.Println("Missing scheme choice")
@@ -113,9 +111,8 @@ var generateCmd = &cobra.Command{
 
 				// Pick a certain hue
 				hue := rand.Intn(360)
-				// Create an array to store the 5 color palette
 
-				// Loop through and add all 5 colors with random saturation and values
+				// Loop through and add all 5 colors with same hue and random saturation and values (30%-100%)
 				for i:=0; i < 5; i++ {
 					palette[i] = HSVColor{hue, rand.Intn((100-30) + 1) + 30, rand.Intn((100-30) + 1) + 30}	
 				}
@@ -123,43 +120,51 @@ var generateCmd = &cobra.Command{
 
 			case "A":
 				// Analgous
-
+				
+				// Set the middle color as the base color
 				palette[2] = HSVColor{rand.Intn(360), rand.Intn((100-80) + 1) + 80, rand.Intn((100-80) + 1) + 80}	
 				baseHue := palette[2].h 
+				// The next 4 colors are 30 degrees away from base hue or 30 degrees away from the adjacent color
 				palette[0] = HSVColor{fixAngle(baseHue - 60), rand.Intn((100-80) + 1) + 80, rand.Intn((100-80) + 1) + 80}	
 				palette[1] = HSVColor{fixAngle(baseHue - 30), rand.Intn((100-80) + 1) + 80, rand.Intn((100-80) + 1) + 80}	
 				palette[3] = HSVColor{fixAngle(baseHue + 30), rand.Intn((100-80) + 1) + 80, rand.Intn((100-80) + 1) + 80}	
 				palette[4] = HSVColor{fixAngle(baseHue + 60), rand.Intn((100-80) + 1) + 80, rand.Intn((100-80) + 1) + 80}	
-
-				fmt.Println(palette)
 				
 			case "C":
 				// Complementary
-				palette[0] = HSVColor{rand.Intn(360), rand.Intn((100-80) + 1) + 80, rand.Intn((100-80) + 1) + 80}	
+
+				// Pick a random color
+				palette[0] = HSVColor{rand.Intn(360), rand.Intn((100-80) + 1) + 80, rand.Intn((100-80) + 1) + 80}
+				// Pick the hue directly opposite to it
 				palette[3] = HSVColor{fixAngle(palette[0].h + 180), rand.Intn((100-80) + 1) + 80, rand.Intn((100-80) + 1) + 80}
+				// Fill the rest of the color palette with 2 of the base hues and 1 of the opposite hues
 				palette[1] = HSVColor{palette[0].h, rand.Intn((100-40) + 1) + 40, rand.Intn((100-40) + 1) + 40}
 				palette[2] = HSVColor{palette[0].h, rand.Intn((100-40) + 1) + 40, rand.Intn((100-40) + 1) + 40}
 				palette[4] = HSVColor{palette[3].h, rand.Intn((100-40) + 1) + 40, rand.Intn((100-40) + 1) + 40}
-
-				fmt.Println(palette)
 			case "T":
 				// Triadic
-				
+
+				// Find a base color
 				palette[0] = HSVColor{rand.Intn(360), rand.Intn((100-80) + 1) + 80, rand.Intn((100-80) + 1) + 80}
+				// Add 120 degrees and subtract 120 degrees from the base hue to get the triadic colors
 				palette[1] = HSVColor{fixAngle(palette[0].h - 120), rand.Intn((100-80) + 1) + 80, rand.Intn((100-80) + 1) + 80}
 				palette[2] = HSVColor{fixAngle(palette[0].h + 120), rand.Intn((100-80) + 1) + 80, rand.Intn((100-80) + 1) + 80}
+				// Choose 1 of the three hues randomly and vary the saturation and value
 				palette[3] = HSVColor{fixAngle(palette[rand.Intn(2)].h),  rand.Intn((100-40) + 1) + 40, rand.Intn((100-40) + 1) + 40}
 				palette[4] = HSVColor{fixAngle(palette[rand.Intn(2)].h),  rand.Intn((100-40) + 1) + 40, rand.Intn((100-40) + 1) + 40}
 				fmt.Println(palette)
 			case "S":
 				// Square
+
+				// Find a base color
 				palette[0] = HSVColor{rand.Intn(360), rand.Intn((100-80) + 1) + 80, rand.Intn((100-80) + 1) + 80}
+				// Add 90 degrees unitl you find all four colors
 				palette[1] = HSVColor{fixAngle(palette[0].h + 90), rand.Intn((100-80) + 1) + 80, rand.Intn((100-80) + 1) + 80}
 				palette[2] = HSVColor{fixAngle(palette[0].h - 90), rand.Intn((100-80) + 1) + 80, rand.Intn((100-80) + 1) + 80}
 				palette[3] = HSVColor{fixAngle(palette[0].h + 180), rand.Intn((100-80) + 1) + 80, rand.Intn((100-80) + 1) + 80}
 				fmt.Println(palette)
 			default:
-				// Best looking color scheme out of the 5 is default
+				fmt.Println("Incorrect scheme choice, please choose from (M)onochromatic, (A)nalogous, (C)omplementary, (T)riadic, or (S)quare")
 		}
 
 		// Print out the palette with colored output
@@ -168,7 +173,8 @@ var generateCmd = &cobra.Command{
 			if r == 0 && g == 0 && b == 0{
 				continue
 			}
-			color.RGB(uint8(r), uint8(g), uint8(b)).Println("rgb: (", r, g, b, ")")
+			output := fmt.Sprintf("rgb(%d, %d, %d)", r, g, b)
+			color.RGB(uint8(r), uint8(g), uint8(b)).Println(output)
 		}
 	},
 }
